93e050fa0dae6d2809858b6fdef797df46633bbf
fernandovlima <fernandovlima.s@gmail.com>
2019-11-18 22:06:43 -0300

    layouts

---
+++ src/pages/_layouts/auth/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: linear-gradient(-50deg, #ee4e64, #f296a2);
+`;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 // pages
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Registration from '../pages/Registration';
+import Dashboard from '../pages/Dashboard';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={SignIn} />
       <Route path="/register" component={SignUp} />
-      <Route path="/registration" component={Registration} />
+      <Route path="/registration" component={Registration} isPrivate />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 }
